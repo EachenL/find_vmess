92b202aaaa301e41323e65c08fd72bdfce84663f
Shuangdi Ning <shundyning@gmail.com>
2023-11-18 16:19:13 +0800

    Create find_vmess.sh
    init

---
+++ find_vmess.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 启动 v2ray 并发送选择命令，然后获取输出
+output=$(echo "3" | v2ray)
+
+# 使用 awk 处理输出，提取完整的 vmess:// 链接
+vmess_link=$(echo "$output" | awk '/vmess:\/\//{print $0; exit}')
+
+# 打印 vmess 链接
+echo "找到的 vmess 链接: $vmess_link"
